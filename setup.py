9cd555a6bfa3900dd07e7af7e0c2b30e4ad6e055
Andrej Kiruscanka <kiruscanka@cshub.cz>
2025-01-27 12:45:11 +0100

    set name to crczp and refactor actions

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ def read(filename):
 
 
 setup(
-    name='cyberrangecz-topology-definition',
+    name='crczp-topology-definition',
     author='cybersecurityhub.cz',
     description='topology-definition',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    packages=find_namespace_packages(include=['cyberrangecz.*'], exclude=['tests']),
+    packages=find_namespace_packages(include=['crczp.*'], exclude=['tests']),
     install_requires=[
         'yamlize>=0.6',
         'structlog>=19.2',
